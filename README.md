4ee53e5b0d71f87b5c834689805f15aeca27e1e7
stovein <tlhdgrl@gmail.com>
2020-11-10 21:37:51 +0300

    progress update

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
 # todo-app
 
-https://v3.vuejs.org/guide/instance.html#creating-an-application-instance
+https://v3.vuejs.org/guide/component-custom-events.html#event-names
 https://v3.vuejs.org/style-guide/#rule-categories
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,78 @@
 <template>
-<h1>Vue 3 Todo App</h1>
-<NewTodoForm :addNewTodo="addNewTodo" :newTodo="newTodo" />
-<button @click="markAllDone">Mark All Done</button>
-<button @click="deleteAll">Erase the list</button>
-<TodoList :todos="todos" :removeTodo="removeTodo" />
-<ConditionalRendering />
-<Lifecycles />
+	<h1>Vue 3 Todo App</h1>
+	<NewTodoForm :addNewTodo="addNewTodo" :newTodo="newTodo" />
+	<button @click="markAllDone">Mark All Done</button>
+	<button @click="deleteAll">Erase the list</button>
+	<TodoList :todos="todos" :removeTodo="removeTodo" />
+	<ConditionalRendering />
+	<Lifecycles />
 </template>
 
 <script>
-import ConditionalRendering from './components/ConditionalRendering'
-import TodoList from './components/TodoList/TodoList'
-import NewTodoForm from './components/TodoForm/NewTodoForm'
-import Lifecycles from './components/Lifecycles'
-import {
-    ref
-} from 'vue';
+import ConditionalRendering from "./components/ConditionalRendering";
+import TodoList from "./components/TodoList/TodoList";
+import NewTodoForm from "./components/TodoForm/NewTodoForm";
+import Lifecycles from "./components/Lifecycles";
+import { ref } from "vue";
 
 export default {
-    setup() {
-        const todos = ref([]);
+	setup() {
+		const todos = ref([]);
 
-        const addNewTodo = (content) => {
-            todos.value.push({
-                id: Date.now(),
-                done: false,
-                content: content
-            });
-        };
+		const addNewTodo = (content) => {
+			todos.value.push({
+				id: Date.now(),
+				done: false,
+				content: content,
+			});
+		};
 
-        const removeTodo = (index) => {
-            todos.value.splice(index, 1);
-        };
+		const removeTodo = (index) => {
+			todos.value.splice(index, 1);
+		};
 
-        const markAllDone = () => {
-            todos.value.forEach((todo) => {
-                todo.done = true;
-            });
-        };
+		const markAllDone = () => {
+			todos.value.forEach((todo) => {
+				todo.done = true;
+			});
+		};
 
-        const deleteAll = () => {
-            todos.value = [];
-        };
+		const deleteAll = () => {
+			todos.value = [];
+		};
 
-        return {
-            todos,
-            removeTodo,
-            markAllDone,
-            deleteAll,
-            addNewTodo,
-        }
-    },
-    components: {
-        ConditionalRendering,
-        TodoList,
-        NewTodoForm,
-        Lifecycles,
-    }
-}
+		return {
+			todos,
+			removeTodo,
+			markAllDone,
+			deleteAll,
+			addNewTodo,
+		};
+	},
+	components: {
+		ConditionalRendering,
+		TodoList,
+		NewTodoForm,
+		Lifecycles,
+	},
+};
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 
 .done {
-    text-decoration: line-through;
+	text-decoration: line-through;
 }
 
 .todo {
-    cursor: pointer;
+	cursor: pointer;
 }
 </style>
